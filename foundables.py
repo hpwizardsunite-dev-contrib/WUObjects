ac0bc05c0bfb7ca1622341ecc2523164da9d28ab
virodoran <1269298+virodoran@users.noreply.github.com>
2019-05-12 19:54:33 -0700

    Added ingredients, runestones, and updated foundables

--- foundables.py
+++ foundables.py
@@ -2,14 +2,14 @@
 import json
 
 class Foundable:
-    def __init__(self):
-        self.id = ''
-        self.family_id = ''
-        self.page_id = ''
-        self.rarity = ''
-        self.name = ''
-        self.desc = ''
-        self.image = ''
+    id = ''
+    name = ''
+    desc = ''
+    icon = ''
+    family_id = ''
+    page_id = ''
+    rarity = ''
+    counts = []
 
     def __str__(self):
         # format can be anything you want
@@ -22,7 +22,6 @@ class Foundables:
         self.s = s
         self.objects = []
 
-    def extract(self):
         for message in self.gdw['messages']:
             if 'collectionItem' in message:
                 foundable = Foundable()
@@ -30,6 +29,7 @@ class Foundables:
                 foundable.family_id = message['collectionItem']['familyId']
                 foundable.page_id = message['collectionItem']['pageId']
                 foundable.rarity = message['collectionItem']['rarity']
+                foundable.counts = message['collectionItem']['shardCounts']
                 self.objects.append(foundable)
 
         for foundable in self.objects:
@@ -37,5 +37,6 @@ class Foundables:
                 if 'collectionItem' in message and message['collectionItem']['id'] == foundable.id:
                     foundable.name = self.s.find(message['collectionItem']['name'])
                     foundable.desc = self.s.find(message['collectionItem']['description'])
-                    foundable.icon = message['collectionItem']['iconReturned'] # need better way to point to asset
-            print(foundable.name + ' - ' + str(foundable.rarity))
+                    foundable.icon = message['collectionItem']['iconReturned']
+            if len(foundable.counts) == 4 and foundable.counts[0] == 1 and foundable.counts[1] == 2 and foundable.counts[2] == 3 and foundable.counts[3] == 4:
+                print(foundable.id)
---
+++ runestones.py
@@ -0,0 +1,48 @@
+class Runestone:
+    id = ''
+    name = ''
+    desc = '' # All the same
+    icon = ''
+    family = ''
+    limit = 0 # All the same
+    level = 0
+    deletable = False
+    show_in_menu = False
+
+class Runestones:
+    def __init__(self, gdc, gdw, s):
+        self.gdc = gdc
+        self.gdw = gdw
+        self.s = s
+        self.objects = []
+
+        for message in self.gdw['messages']:
+            if 'vaultItem' in message and message['vaultItem']['vaultCategoryGmtId'] == 'proto_vaultcategory_runestones':
+                obj = message['vaultItem']
+                runestone = Runestone()
+                runestone.id = obj['id']
+                runestone.limit = int(obj['cap'])
+                runestone.level = int(obj['runestoneItem']['quality'])
+                runestone.family = obj['runestoneItem']['collectionFamilyGmtId']
+                self.objects.append(runestone)
+
+        for runestone in self.objects:
+            for message in self.gdc['messages']:
+                # First replace the family ID with the family name
+                if 'collectionFamily' in message and message['collectionFamily']['id'] == runestone.family:
+                    obj = message['collectionFamily']
+                    runestone.family = self.s.find(obj['name'])
+                    # TODO: Could get family icon here as well
+
+                # Then get the rest
+                if 'vaultItem' in message and message['vaultItem']['id'] == runestone.id:
+                    obj = message['vaultItem']
+                    runestone.icon = obj['icon']
+                    if 'deletable' in obj:
+                        runestone.deletable = obj['deletable']
+                    if 'showInVaultItemMenu' in obj:
+                        runestone.show_in_menu = obj['showInVaultItemMenu']
+
+                    runestone.name = self.s.find(obj['name'])
+                    runestone.desc = self.s.find(obj['description'])
+
